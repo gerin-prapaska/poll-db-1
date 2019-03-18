7f8848f002b395129deab66c4367d3826c3fdfec
mar-sh <marasi.af@gmail.com>
2019-03-18 11:13:52 +0700

    r0

--- setup.js
+++ setup.js
@@ -1,1 +1,41 @@
 //your code here
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./poll.db');
+
+db.serialize(function(){
+  db.run(`CREATE TABLE IF NOT EXISTS politicians (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    party TEXT,
+    location TEXT,
+    grade_current REAL
+    )`)
+  });
+    
+
+db.serialize(function(){
+  db.run(`CREATE TABLE IF NOT EXISTS voters(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    gender TEXT,
+    age INTEGER
+  )`)
+});
+
+db.serialize(function(){
+  db.run(`CREATE TABLE IF NOT EXISTS votes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    voterId INTEGER,
+    politicianId INTEGER
+  )`)
+});
+
+db.close(function(err){
+  if(err){
+    console.log(err)
+  } else {
+    console.log('success')
+  }
+})
+
